34ca1e2497870e9ae2ade59019d8c7f5e170c4a7
flippedtortoise <flippedtortoise@gmail.com>
2021-01-28 19:45:59 -0700

    first real commit, first website i practiced html, css, and JS together

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/haro-phone.jpeg') {
+      myImage.setAttribute('src','images/IKNOWYOUREHERE.png');
+    } else {
+      myImage.setAttribute('src','images/haro-phone.jpeg');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+    myHeading.textContent = `Hello world, ${myName}`;
+}
+}
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Hello world, ' + storedName;
+}
+myButton.onclick = function() { 
+    setUserName();
+}
